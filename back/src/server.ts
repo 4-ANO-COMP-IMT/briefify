e478b15bfd5a4c57cfb62f6211ed25d41a3a659a
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-26 11:16:49 -0300

    feat(back): express config

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,14 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import user from "./controllers/user";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(user);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
