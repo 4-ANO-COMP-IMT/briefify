a095ccc3cb696befd85a7631d84ef5d2d2c3054b
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-31 14:46:33 -0300

    feat: endpoint `/status`

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import user from "./controllers/user";
+import status from "src/routes/status";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(user);
+app.use(status);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
