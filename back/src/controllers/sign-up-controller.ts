6b592c372fafba2ddc2d2d56389bb846a6fdeb02
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-01 00:58:00 -0300

    refat: split auth-controller in `sign-in-controller` and `sign-up-controller`

--- back/src/controllers/sign-up-controller.ts
+++ back/src/controllers/sign-up-controller.ts
@@ -12,13 +12,13 @@ const SignUpSchema = z.object({
   role: z.string().optional(),
 });
 
-const signUpUser = async (req: Request, res: Response) => {
+const signUp = async (req: Request, res: Response) => {
   try {
     const bodyValidated = SignUpSchema.parse(req.body);
     const response = await database.query(
       `INSERT INTO "User" (id, name, cpf, email, password, company, role)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-    RETURNING id, name, email, company, role;`,
+       VALUES ($1, $2, $3, $4, $5, $6, $7)
+       RETURNING id, name, email, company, role;`,
       [
         uuid(),
         bodyValidated.name,
@@ -29,7 +29,7 @@ const signUpUser = async (req: Request, res: Response) => {
         bodyValidated.role || null,
       ],
     );
-    console.log(response);
+
     const createdUser = response?.rows[0];
     res.status(201).json(createdUser);
   } catch (error) {
@@ -42,4 +42,4 @@ const signUpUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { signUpUser };
+export default { signUp };
--- back/src/routes/sign-up.ts
+++ back/src/routes/sign-up.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import authController from "src/controllers/auth-controller";
+import signUpController from "src/controllers/sign-up-controller";
 
 const router = express.Router();
 
-router.route("/sign-up").post(authController.signUpUser);
+router.route("/sign-up").post(signUpController.signUp);
 
 export default router;
