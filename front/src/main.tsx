9c140a5bd930f2444f99efc339aead6643c130c0
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-02 23:13:20 -0300

    feat(front): implemented react-router-dom

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/home.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "./pages/sign-in.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
