89e7773906330feef38b3ff1d6a005eeb1ac2a55
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-02 23:14:06 -0300

    refat(front): navbar logo

--- front/src/components/navbar.tsx
+++ front/src/components/navbar.tsx
@@ -1,11 +1,11 @@
-import { NotepadText } from "lucide-react";
+import { AudioWaveform } from "lucide-react";
 import { Button } from "./ui/button";
 
 export function Navbar() {
   return (
     <div className="bg-transparent fixed top-0 w-full px-6 py-4 flex flex-row items-center justify-between z-20 backdrop-blur-sm">
       <div className="flex flex-row gap-2 items-center">
-        <NotepadText strokeWidth={1.5} className="h-5 w-5" />
+        <AudioWaveform strokeWidth={1.5} className="h-5 w-5" />
         <p className="text-primary font-semibold">Briefify</p>
       </div>
       <div className="flex flex-row gap-2 items-center">
