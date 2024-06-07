fe69042d62d0db7d53e25a9e5e7d3018141a2570
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-06-07 02:13:09 -0300

    feat(front): sign-up ui

--- front/src/pages/sign-up.tsx
+++ front/src/pages/sign-up.tsx
@@ -5,194 +5,65 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-// import axios from "axios";
-
-const RegisterSchema = z
-  .object({
-    name: z.string().min(3, { message: "Nome Invalido." }),
-    cpf: z.string({ message: "CPF Invalido." }),
-    company: z.string().optional(),
-    role: z.string().optional(),
-    email: z.string().email({ message: "Email Invalido." }),
-    password: z.string(),
-    passwordConfirm: z.string(),
-  })
-  .refine(
-    (data) =>
-      data.password.match(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      ),
-    {
-      message:
-        "A senha deve ter pelo menos 8 caracteres, conter uma letra maiúscula, uma letra minúscula, um número e um caractere especial.",
-      path: ["password"],
-    },
-  )
-  .refine((data) => data.passwordConfirm === data.passwordConfirm, {
-    message: "A senha e a confirmação de senha devem ser iguais.",
-    path: ["password", "passwordConfirm"],
-  });
+import { Building2, PersonStanding } from "lucide-react";
+import { CardButton } from "@/components/ui/card-button";
+import { ReactNode, useEffect, useState } from "react";
+import RegisterForm from "@/components/register-form";
 
 export default function SignIn() {
-  const form = useForm<z.infer<typeof RegisterSchema>>({
-    resolver: zodResolver(RegisterSchema),
-    defaultValues: {
-      name: "",
-      cpf: "",
-      company: "",
-      role: "",
-      email: "",
-      password: "",
-      passwordConfirm: "",
-    },
-  });
+  const [accountType, setAccountType] = useState<
+    "personal" | "cooperative" | undefined
+  >();
+  const [cardContent, setCardContent] = useState<ReactNode>(
+    <AccountTypeCardContent accountType={setAccountType} />,
+  );
 
-  async function onSubmit(values: z.infer<typeof RegisterSchema>) {
-    console.log(values);
-    //   const response = await axios({
-    //     method: "post",
-    //     url: "http://localhost:3000/sign-up",
-    //     data: {
-    //       name: values.name,
-    //       cpf: values.cpf,
-    //       company: values.company ?? null,
-    //       role: values.role ?? null,
-    //       email: values.email,
-    //       password: values.password,
-    //     },
-    //   });
-    //   console.log(response.data);
-  }
+  useEffect(() => {
+    if (accountType) {
+      setCardContent(<RegisterForm accountType={accountType} />);
+    }
+  }, [accountType]);
 
   return (
-    <div className="w-screen h-screen bg-muted flex items-center justify-center">
-      <Card className="w-10/12 md:max-w-lg">
-        <CardHeader>
-          <CardTitle>Criar conta</CardTitle>
-          <CardDescription>
-            Junte-se a nós e transforme a eficiência de suas reuniões.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex flex-col gap-5"
-            >
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>Nome Completo:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Fulano Silva" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="cpf"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>CPF:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="XXX.XXX.XXX-XX" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="company"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>Empresa:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Fulano Tech" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="role"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>Cargo:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Desenvolvedor" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="exemplo@email.com" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel>Senha</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="******"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <Button type="submit" className="w-full">
-                Cadastrar
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+    <div
+      className={`w-screen ${accountType === "cooperative" ? "h-full py-32" : "h-screen"} bg-muted flex items-center justify-center`}
+    >
+      <Card className="w-10/12 md:w-fit">{cardContent}</Card>
     </div>
   );
 }
+
+interface AccountTypeCardContent {
+  accountType: (type: "personal" | "cooperative") => void;
+}
+
+const AccountTypeCardContent = ({ accountType }: AccountTypeCardContent) => {
+  return (
+    <>
+      <CardHeader>
+        <CardTitle>Qual é o tipo da conta?</CardTitle>
+        <CardDescription>Selecione o propósito da sua conta.</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col md:flex-row gap-3">
+        <CardButton
+          onClick={() => accountType("personal")}
+          icon={PersonStanding}
+          title={"Pessoal"}
+          description={
+            "Conta para uso pessoal, ideal para suas atividades pessoais e hobbies."
+          }
+          bottomText="Próxima Etapa"
+        />
+        <CardButton
+          onClick={() => accountType("cooperative")}
+          icon={Building2}
+          title={"Corporativa"}
+          description={
+            "Conta para empresas, perfeita para administrar atividades profissionais e empresariais."
+          }
+          bottomText="Próxima Etapa"
+        />
+      </CardContent>
+    </>
+  );
+};
