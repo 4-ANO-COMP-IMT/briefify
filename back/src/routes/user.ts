969f27b90af6c5ed573c3e84b96bfaac189d4a8a
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-09-02 01:19:19 -0300

    feat: user route

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -4,5 +4,6 @@ import userController from "src/controllers/user-controller";
 const router = express.Router();
 
 router.route("/user").get(userController.getAllUsers);
+router.route("/user/:id").get(userController.getUserById);
 
 export default router;
