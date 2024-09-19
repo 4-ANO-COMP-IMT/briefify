e40ae15ffb10c624aff33940e01a2767c014ded3
GabrielBianconiconi <gabrielbianconiconi@gmail.com>
2024-09-19 16:40:18 -0300

    refat: add transcription resume to RabbitMQ

--- back/src/transcription/index.ts
+++ back/src/transcription/index.ts
@@ -1,33 +1,54 @@
 import cors from "cors";
 import express from "express";
-
+import resumeRoutes from "src/transcription/routes/resume";
 import { environments } from "src/shared/env/environments";
-import resume from "src/transcription/routes/resume"
+import { Subscriber } from "src/shared/RabbitMQ/subscriber";
+import { Server } from "socket.io";
+import handleSocketEvents from "../socket/socket";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Rotas
+app.use('/', resumeRoutes);
 
 
-const server = async () => {
+// Inicializando subscribers
+const TranscriptionCreatedSubscriber = new Subscriber('signup_queue', (message) => {
+  console.log('Processed signup event:', message);
+});
+TranscriptionCreatedSubscriber.connect();
 
-    const PORT = environments.TRANSCRIPTION_PORT
+const SummaryGeneratedSubscriber = new Subscriber('signin_queue', (message) => {
+  console.log('Processed signin event:', message);
+});
+SummaryGeneratedSubscriber.connect();
 
-    const app = express()
-    app.use(express.json())
-    app.use(cors())
 
-    app.use(resume);
 
-    
-    app.get('/', (req, res) => {
-        res.send('API is running.')
-    })
+// Inicializando o servidor
+const PORT = environments.TRANSCRIPTION_PORT || 5000;
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-    app.post('/eventBus', (req, res) => {
-        console.log('eventBus is listening.')
-        res.status(200).send({ msg: 'OK' })
-    })
+// Fechando conexões do RabbitMQ ao encerrar o servidor
+process.on('SIGINT', async () => {
+  console.log('Closing RabbitMQ connections...');
+  await TranscriptionCreatedSubscriber.close();
+  await SummaryGeneratedSubscriber.close();
+  server.close(() => {
+    console.log('Server shut down');
+    process.exit(0);
+  });
+});
 
-    app.listen(PORT, () => {
-        console.log(`Transcription is running on port ${PORT}`)
-    })   
-}
-server()
+const io = new Server(server, {
+  cors: {
+    origin: [`http://localhost:5173`, `http://localhost:5174`],
+    methods: ["GET", "POST"],
+  },
+});
 
+handleSocketEvents(io);
